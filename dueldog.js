6bd34f591f32b95a7de0ca755057ff6aaa038a16
Hoon Chang <hoonchang124@gmail.com>
2020-05-28 18:59:00 -0400

    commit 19

--- dueldog.js
+++ dueldog.js
@@ -1,11 +1,8 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    // is mobile..
-    document.write("<html>
-  <head><title>Website</title></head>
-  <body>
-     <h1>It seems like you're on a mobile device, please visit us on your PC!</h1>
-  </body>
-  </html>");
+
+  document.open();
+  document.write("<h1>It seems like you're on a mobile device, please visit us on your PC!</h1>");
+  document.close();
 }
 else {
     document.write("<p><b>You're on PC</b></p>");
