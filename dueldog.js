06ddb3b8616d92aa7bf2341a532bdbf11d1028fd
Hoon Chang <hoonchang124@gmail.com>
2020-05-28 17:54:01 -0400

    13th commit

--- dueldog.js
+++ dueldog.js
@@ -1,7 +1,7 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     // is mobile..
-    document.write("<p><b> Commit 12 - Plese visit us on your PC!</b></p>");
+    document.write("<p><b> Commit 13 - Plese visit us on your PC!</b></p>");
 }
 else {
-    document.write("<p><b> Commit 12 - You're on PC</b></p>");
+    document.write("<p><b> Commit 13 - You're on PC</b></p>");
 }
