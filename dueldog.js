a13af97403d8ef8297899b9c241c876a2ca9b01e
Hoon Chang <hoonchang124@gmail.com>
2020-05-28 17:14:26 -0400

    third commit

--- dueldog.js
+++ dueldog.js
@@ -1,4 +1,12 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     // is mobile..
-    document.write("<h2> You're on mobile! </h2>");
+    document.write("<div id="splashscreen"> <h2>Please visit us on your PC!/h2> </div>");
+}
+else( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // is mobile..
+    document.write("<img src="logo.png" alt="logo"> <h1>DUELDOG</h1>
+    <p>Current Beta Test Full
+    Next beta beginning soon!
+    Enter your email, our admin team will contact
+    you shortly with additional information!</p>");
 }
