f480ef38f486c9594f4356eb9a01498e1229fa6c
Hoon Chang <hoonchang124@gmail.com>
2020-06-16 12:02:09 -0400

    commit 104

--- dueldog.js
+++ dueldog.js
@@ -1,4 +1,3 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-
   window.location.replace('https://hoonchang.github.io/dueldog/mobile.html');
 }
